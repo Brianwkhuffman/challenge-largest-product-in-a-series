528d13bf4cd666f3f59923a11e07c71376cc1a22
Briii <brianwkhuffman@gmail.com>
2019-09-21 10:01:12 -1000

    Finished

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,25 @@
 class LargestProduct {
   constructor(number) {
+    this.number = number;
+    this.index = 0;
+  }
+  getLargestProduct() {
+    let temp = 0;
+    let arr = this.number.split('');
+    for (let i = 0; i < arr.length; i++) {
+      let x = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
+      let y = arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4] * arr[i + 5];
+      if (temp < x && x > y) {
+        temp = x;
+        this.index = i
+      }
+    }
+    return temp;
+  }
 
+  getLargestProductIndex() {
+    this.getLargestProduct();
+    return this.index;
   }
 }
 
